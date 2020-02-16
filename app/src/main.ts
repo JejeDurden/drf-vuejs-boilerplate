7c9beb45990c0ecc31cdd6838d1d404f327b1116
Jérôme <jerome.desmares@privateaser.com>
2020-02-16 17:01:03 +0100

    update packages

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import "./quasar";
 
 Vue.config.productionTip = false;
 
